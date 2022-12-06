9bb9f438d0a8425ccfa818dd6954bf637bc71f4c
Poojabodake2001 <poojabodake5284@gmail.com>
2022-12-06 13:47:55 +0530

    primitive datatype-string interpolation

---
+++ assets/js/js app.js
@@ -0,0 +1,222 @@
+var fname = "Pooja";
+var lname = "Bodake";
+var age = 21;
+var nativePlace = "Kolhapur";
+
+// Hello there I'm Pooja Bodake, 21 years old. I'm from Kolhapur 
+
+var intro = "Hello there I'm " + fname + " " + lname + ", " + age +
+    " years old. I'm from " + nativePlace
+
+console.log(intro);
+document.write(intro);
+document.write("<hr>");
+
+
+var fname = "Virat";
+var lname = "Kohli";
+var profile = "Indian international cricketer and former captin of the India national cricket team";
+var team = "delhi";
+var teamName = "domestic cricket and Royal"
+
+//Virat Kohli is an Indian international cricketer and former captain of the India national cricket team.
+//He plays for Delhi in domestic cricket and Royal.
+
+var intro = "<p>" + fname + " " + lname + " is an " + profile + ". He plays for " + team + " in " + teamName + ".</p>"
+
+console.log(intro);
+document.write(intro);
+document.write("<hr>");
+
+
+var fname = "sachin";
+var lname = "Tendulakar";
+var bornPlace = "Niramal Nursing Home in Dadar, Bombay";
+var birthDate = "24 April 1973";
+var fatherName = "Ramesh";
+var fatherProfile = "Well-known Marathi novelist & poet";
+var motherName = "Rajni";
+var motherProfile = "worked in unsurance industry";
+
+// Sachin Tendulkar was born at Nirmal Nursing Home in Dadar, Bombay on 24 April 1973.
+// His father, Ramesh Tendulkar, was a well-known Marathi novelist & poet and his mother, Rajni,
+// worked in the insurance industry.
+
+var info = "<p>" + fname + " " + lname + " was born at " + bornPlace + " on " + birthDate + "." + " His father,"
+    + fatherName + ", " + "was a " + fatherProfile + " and his mother, " + motherName + ", " + motherProfile + ".</p>"
+
+console.log(info);
+document.write(info);
+document.write("<hr>");
+
+var fname = "Ratan";
+var lname = "Tata";
+var birthDate = "28 December 1937";
+var profile = "Indian industrialist, and a former chairman of Tata Sons";
+var chairmanDuration = 1990 - 2012;
+var firstAward = "Padma Vibhushan (2008)";
+var secondAward = "Padma Bhushan (2000)"
+
+//Ratan Tata (born 28 December 1937) is an Indian industrialist, and a former chairman of Tata Sons.
+// He was the chairman of Tata from 1990 to 2012, He is the recipient of two civilian awards of India, the Padma Vibhushan (2008) 
+//the second highest civilian honour, and the Padma Bhushan (2000).
+
+var info = "<p>" + fname + " " + lname + " (born " + birthDate + " ) " + " is an " + profile + "." + " He was the chairman of Tata from " + chairmanDuration +
+    "," + " He is the recipient of two civilian awards of india, the " + firstAward + "the second highest civilian honour, and the "
+    + secondAward + ".</p>"
+
+console.log(info);
+document.write(info);
+document.write("<hr>");
+
+var fname = "Sunder";
+var lname = "Pichai";
+var profile = "Chief Executive Officer (CEO) of Google Inc";
+var born = "June 10, 1972";
+var education = "IIT Kharagpur in metallurgical engineering";
+var spouse = "Anjali Pichai";
+var children = 2;
+var award = "Padma Bhushan"
+
+//Sundar Pichai is a successful  Chief Executive Officer (CEO) of Google Inc.
+// He was born June 10, 1972.his degree from IIT Kharagpur in metallurgical engineering.
+//Pichai is married to Anjali Pichai and has two children. Pichai received the Padma Bhushan.
+
+var info = "<p>" + fname + " " + lname + " is a successful " + profile + "." + "He was born " + born + "." + " his degree from "
+    + education + "." + lname + " is married to " + spouse + " and has " + children + "." + lname + " received the " + award + ". </p>"
+
+console.log(info);
+document.write(info);
+document.write("<hr>");
+
+
+
+var fname = "Amitabh";
+var lname = "Bachchan";
+var profile = "Actor";
+var born = "October 11, 1942";
+var nativePlace = "Allahabad";
+var age = 80;
+var spouse = "Jaya";
+var childName = "Abhishek"
+
+
+//Amitabh bachchan is an actor, bachchan was born in October 11, 1942 and from Allahabad.he 80 years old.his wife name is Jaya Bachchan.
+// he have son, whose name is Abhishek Bachchan, 
+
+
+var info = "<p class='alert alert-success'>" + fname + " " + lname + " is an " + profile + ", Bachchan was born in "
+    + born + " and from " + nativePlace + " he " + age + " years old his wife name is " + spouse + " " +
+    lname + " he have son, whose name is " + childName + ". </P>"
+
+console.log(info);
+document.write(info);
+document.write("<hr>");
+
+
+
+
+
+
+
+var fname = "kareena ";
+var lname = "kapoor";
+var age = 42;
+var profile = "Actor";
+var spouse = "Saif Ali Khan";
+var nativePlace = "Mumbai";
+var fatherName = " Randhir Kapoor";
+var childName = "Taimur ";
+
+// Kareena Kapoor is a Actor, He is 42 years old.
+//kareena husband name is Saif Ali Khan and from Mumbai.kareena father name is  Randhir Kapoor.
+// she is having a baby boy whose name is Taimur Khan,
+
+
+var info = "<p> Hello There I'm " + fname + " " + lname + "," + age +
+    " years old. kareena husband is " + spouse + " and from " + nativePlace + "." + " kareena father name is " + fatherName +
+    ". I have one baby boy, whose name is " + childName + " " + lname + ". </p>"
+
+console.log(info);
+document.write(info);
+document.write("<hr>");
+
+
+
+
+
+
+var fname = "Donald";
+var lname = "Trump";
+var age = 76;
+var profile = " American politician";
+var born = "June 14, 1946";
+var spouse = " Melania Trump";
+var nativePlace = "New York";
+var daughterName = "Ivanka";
+
+//Donald Trump is an American Politician, Trmp was born June 14, 1946 and from New York.he 76 years old.
+//his wife name is Melania Trump.he have daughter, whose name is Ivanka Trump
+
+
+var info = "<p>" + fname + " " + lname + " is an " + profile + ", Trump was born in " + born + " and from "
+    + nativePlace + " he " + age + " years old his wife name is " + spouse + " " +
+    lname + " he have daughter, whose name is " + daughterName + ". </P>"
+
+
+console.log(info);
+document.write(info);
+document.write("<hr>");
+
+
+
+
+
+var fname = "Indira";
+var lname = "Gandhi";
+var born = "19 November 1917";
+var profile = "Indian Politician";
+var age = 66;
+var nativePlace = "Prayagraj";
+var sonName = "Rajiv";
+var Died = "21 May 1991";
+var grandChild = "Rahul";
+
+
+//Indira Gandhi is an Indian Politician, she was born 19 November 1917 and from Prayagraj.he 66 years old.
+// she have son, whose name is Rajiv Gandhi, He died 21 May 1991. he is having a son whose name is Rahul Gandhi,
+
+var info = "<p>" + fname + " " + lname + " is an " + profile + ", she was born in " + born + " and from " + nativePlace + " he " + age + " years old " +
+    " she have son, whose name is " + sonName + " " + lname + " Rajiv died was " + Died + " he is having a son whose name is " + grandChild + " " +
+    lname + ". </P>"
+
+console.log(info);
+document.write(info);
+document.write("<hr>");
+
+
+
+
+var fname= "Rohit";
+var lname= "Sharma";
+var age= 35;
+var born= " born on 30 April 1987";
+var profile= "Indian international cricketer and the current captain of the Indian cricket team";
+var spouse= " Ritika Sajdeh";
+var birthPlace= "Nagapur";
+var father= " Gurunath ";
+var daughter= "Samaira";
+
+
+//  Rohit Sharma is a Indian international cricketer and the current captain of the Indian cricket team.
+// He is 35 years old.born on 30 April 1987. Rohit wife name Ritika Sajdeh is and from Nagapr. he is having a baby girl whose name is Samaira sharma,
+
+
+var info = "<p>" + fname + " " + lname + " is a " + profile +" He is " + age +
+    " years old. born on  " + born + ". Rohit wife name is " + spouse + " and from " + birthPlace + ". he is having a baby girl whose name is " + daughter +
+     ". </P>"
+
+     console.log(info);
+     document.write(info);
+     document.write("<hr>");
+     
